3e618fb9ea0ea6a7e9835ff5b30e404e3bfda2ae
Son Phan <phantrungsonjoe@gmail.com>
2024-06-20 10:09:45 -0500

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import React from "react";
 import Navigation from "./components/navigation";
+import { Home } from "./components/Home";
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Home />
     </div>
   );
 }
